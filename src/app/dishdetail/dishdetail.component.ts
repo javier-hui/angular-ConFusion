8a1b8205ccffb6aaf9530121b41582ad4f54ae1c
javier <javier.hui@gmail.com>
2020-09-09 02:52:01 +0800

    assignment 3

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { switchMap } from 'rxjs/operators';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Comment } from '../shared/comment';
 
 @Component({
     selector: 'app-dishdetail',
@@ -16,14 +18,34 @@ export class DishdetailComponent implements OnInit {
     dishIds: string[];
     prev: string;
     next: string;
+    commentForm: FormGroup;
+    comment: Comment;
+    @ViewChild('cform') commentFormDirective;
 
-    constructor(private dishService: DishService, private route: ActivatedRoute, private location: Location) { }
+    formErrors = {
+        'author': '',
+        'comment': ''
+    };
+
+    validationMessages = {
+        'author': {
+            'required': 'Author Name is required.',
+            'minlength': 'Author Name must be at least 2 characters long.'
+        },
+        'commment': {
+            'required': 'Comment is required.'
+        },
+    };
+
+    constructor(private dishService: DishService, private route: ActivatedRoute, private location: Location, private cm: FormBuilder) {
+        this.createForm();
+    }
 
     ngOnInit(): void {
         this.dishService.getDishIds()
             .subscribe((dishIds) => this.dishIds = dishIds);
         this.route.params.pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
-            .subscribe((dish) => {this.dish = dish; this.setPrevNext(dish.id)});
+            .subscribe((dish) => { this.dish = dish; this.setPrevNext(dish.id) });
     }
 
     setPrevNext(dishId: string) {
@@ -34,5 +56,53 @@ export class DishdetailComponent implements OnInit {
 
     goBack(): void {
         this.location.back();
+    }
+
+    createForm() {
+        this.commentForm = this.cm.group({
+            author: ['', [Validators.required, Validators.minLength(2)]],
+            comment: ['', [Validators.required]],
+            rating: 5,
+            date: ''
+        });
+        this.commentForm.valueChanges
+            .subscribe(data => this.onValueChanged(data));
+
+        this.onValueChanged(); // reset form validation messages
+    }
+
+    onValueChanged(data?: any) {
+        if (!this.commentForm) { return; }
+        const form = this.commentForm;
+        for (const field in this.formErrors) {
+            if (this.formErrors.hasOwnProperty(field)) {
+                // clear previous error message (if any)
+                this.formErrors[field] = '';
+                const control = form.get(field);
+                if (control && control.dirty && !control.valid) {
+                    const messages = this.validationMessages[field];
+                    for (const key in control.errors) {
+                        if (control.errors.hasOwnProperty(key)) {
+                            this.formErrors[field] += messages[key] + ' ';
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    onSubmit() {
+        this.comment = this.commentForm.value;
+        let now = new Date();
+        this.comment.date = now.toISOString();
+        console.log(this.comment);
+        this.dish.comments.push(this.comment);
+        this.commentFormDirective.resetForm();
+        this.commentForm.reset({
+            author: '',
+            rating: 5,
+            comment: '',
+            date: ''
+        });
     }
 }
