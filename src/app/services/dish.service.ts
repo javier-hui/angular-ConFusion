08eb1bdf30d4ab519bc37b308e319add9417a848
javier <javier.hui@gmail.com>
2020-09-08 11:49:01 +0800

    rxjs part 2

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -25,4 +25,8 @@ export class DishService {
         // simulate server latency with 2-second delay
         return of(DISHES.filter((dish) => dish.featured)[0]).pipe(delay(2000));
     }
+
+    getDishIds(): Observable<string[] | any> {
+        return of(DISHES.map(dish => dish.id));
+    }
 }
