ceddca52d2e783c8e66eecba0dac1a7759588526
javier <javier.hui@gmail.com>
2020-09-17 01:46:35 +0800

    animations part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -6,11 +6,25 @@ import { DishService } from '../services/dish.service';
 import { switchMap } from 'rxjs/operators';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Comment } from '../shared/comment';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
     selector: 'app-dishdetail',
     templateUrl: './dishdetail.component.html',
-    styleUrls: ['./dishdetail.component.scss']
+    styleUrls: ['./dishdetail.component.scss'],
+    animations: [
+        trigger('visibility', [
+            state('shown', style({
+                transform: 'scale(1.0)',
+                opacity: 1
+            })),
+            state('hidden', style({
+                transform: 'scale(0.5)',
+                opacity: 0
+            })),
+            transition('* => *', animate('0.5s ease-in-out'))
+        ])
+    ]
 })
 export class DishdetailComponent implements OnInit {
 
@@ -22,6 +36,7 @@ export class DishdetailComponent implements OnInit {
     commentForm: FormGroup;
     comment: Comment;
     dishcopy: Dish;
+    visibility = 'shown';
 
     @ViewChild('cform') commentFormDirective;
 
@@ -42,13 +57,21 @@ export class DishdetailComponent implements OnInit {
 
     constructor(private dishService: DishService, private route: ActivatedRoute, private location: Location, private cm: FormBuilder, @Inject('baseURL') private BaseURL) {
         this.createForm();
+        this.route.params.pipe(switchMap((params: Params) => { this.visibility = 'hidden'; return this.dishService.getDish(params['id']); }))
+            .subscribe(dish => {
+                this.dish = dish;
+                this.dishcopy = dish;
+                this.setPrevNext(dish.id);
+                this.visibility = 'shown';
+            },
+            errmess => this.errMess = <any>errmess);
     }
 
     ngOnInit(): void {
         this.dishService.getDishIds()
             .subscribe((dishIds) => this.dishIds = dishIds);
         this.route.params.pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
-            .subscribe((dish) => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id)},
+            .subscribe((dish) => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id) },
                 errmess => this.errMess = <any>errmess);
     }
 
@@ -106,11 +129,11 @@ export class DishdetailComponent implements OnInit {
                 this.dish = dish;
                 this.dishcopy = dish;
             },
-            errmess => {
-                this.dish = null;
-                this.dishcopy = null;
-                this.errMess = <any>errmess;
-            });
+                errmess => {
+                    this.dish = null;
+                    this.dishcopy = null;
+                    this.errMess = <any>errmess;
+                });
         this.commentFormDirective.resetForm();
         this.commentForm.reset({
             author: '',
